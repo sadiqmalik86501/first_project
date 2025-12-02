8386f77a56e60bc5d33a5c6c35a81c35d44817d6
Sadiq Malik <sadiqmalik86501@gmail.com>
2025-12-02 11:49:58 +0530

    git add .

--- project_1.py
+++ project_1.py
@@ -7,4 +7,13 @@ data=load_data("data.json")
 print(data)
 
 def display_user(data):
-    
+    print("/n User Informatation/n")
+    for user in data["users"]:
+           print(
+            f"Id.{user["id"]}:{user["name"]}: as with friends{user["friends"]} and like pages{user["liked_pages"]}"
+            )
+    print("\n Page Informatation \n")
+    for page in data["pages"]:
+        print(f"Id,{page["id"]}:{page["name"]}")
+
+display_user(data)
